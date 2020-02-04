1382475a6b15042a0d4fb47ca15d358af7776f32
Kieran Glover <Kieran.glovjunk@gmail.com>
2020-02-04 13:30:26 -0500

    Updated Mars

---
+++ TERRAMARS/Assets/Scripts/boardTile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class boardTile : MonoBehaviour
+{
+
+    public GameObject tile;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseUp()
+    {
+
+        tile.GetComponent<Renderer>().enabled = !(tile.GetComponent<Renderer>().enabled);
+    }
+}
---
+++ TERRAMARS/Assets/Scripts/player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+
+    public int[] resource = new int[6];
+    public int[] resourceProduction = new int[6];
+    public int tr = 20;
+    public int vp;
+    public int cardsInHand = 0;
+    public int cities = 0;
+    public int greeneries = 0;
+    public int specialTiles = 0;
+    public int tags = 0;
+    public bool passed = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
